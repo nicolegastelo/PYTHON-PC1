953056715f094fb427d5556e320b47d202412a45
nicolegastelo <nicolegastelosilva@gmail.com>
2025-07-04 18:33:32 +0000

    Problema 9

---
+++ Problema9.py
@@ -0,0 +1,4 @@
+entrada=input("Ingrese los elementos de la lista separados por espacios: ")
+lista_original = entrada.split()
+lista_invertida = lista_original[::-1]
+print(f"La Lista invertida es la siguiente:{lista_invertida}")
